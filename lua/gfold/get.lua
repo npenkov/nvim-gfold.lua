98c4b83e845b6d2dbb757c49d6e400c5ffd76f0f
Nick Penkov <nick.penkov@gmail.com>
2023-10-22 18:38:49 +0200

    Using plenary. Fix unpulled

--- lua/gfold/get.lua
+++ lua/gfold/get.lua
@@ -1,53 +1,51 @@
-local settings = require('gfold.settings')
-
-local parse_output = function(data)
-  local raw = table.concat(data, '\n')
-  if raw == '' then
-    return {}
-  end
-  local ret = vim.fn.json_decode(raw)
-  for _, v in ipairs(ret) do
-    v.status = v.status:lower()
-    v.path = v.parent .. '/' .. v.name
-    v.remote = v.url
-    v.user = v.email
-  end
-  return ret
-end
-
-local filter = function(repos, condition)
-  local new_repos = {}
-  for _, repo in ipairs(repos) do
-    if condition(repo) then
-      table.insert(new_repos, repo)
-    end
-  end
-  return new_repos
-end
+local settings = require("gfold.settings")
+local Job = require("plenary.job")
 
 local get_repos = function(callback, condition)
-  vim.fn.jobstart('gfold --display-mode json', {
-    cwd = settings.cwd,
-    on_stdout = function(_, data, _)
-      local repos = parse_output(data)
-      if condition then
-        repos = filter(repos, condition)
-      end
-      callback(repos)
-    end,
-    on_stderr = function(_, data, _)
-      if settings.no_error then
-        return
-      end
-      local text = table.concat(data, '\n')
-      if text ~= '' then
-        vim.notify_once('Error (gfold.nvim): ' .. text, vim.log.levels.WARN)
-      end
-    end,
-    stdout_buffered = true,
-  })
+	local jsonStr = ""
+	local parse_result = function(rawJson)
+		if jsonStr == "" then
+			vim.notify_once(
+				"Warning (gfold.nvim): takes too long to fetch the repositories or no repositories found !",
+				vim.log.levels.WARN
+			)
+			return {}
+		end
+		local ret = vim.fn.json_decode(rawJson)
+		for _, v in pairs(ret) do
+			v.status = v.status:lower()
+			v.path = v.parent .. "/" .. v.name
+			v.remote = v.url
+			v.user = v.email
+		end
+		return ret
+	end
+	local job = Job:new({
+		command = "gfold",
+		args = { "--display-mode", "json" },
+		cwd = settings.cwd,
+		enable_recording = false,
+		buffer_output = false,
+		on_stdout = function(_, line, _)
+			jsonStr = jsonStr .. line .. "\n"
+		end,
+		on_stderr = function(_, data, _)
+			if settings.no_error then
+				return
+			end
+			local text = data .. "\n"
+			if text ~= "" then
+				vim.notify_once("Error (gfold.nvim): " .. text, vim.log.levels.WARN)
+			end
+		end,
+	})
+	job:sync(settings.timeout)
+	local res = parse_result(jsonStr)
+	vim.schedule(function()
+		callback(res)
+	end)
 end
 
 return {
-  get_repos = get_repos,
+	get_repos = get_repos,
 }
