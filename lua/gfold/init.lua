9d151ac4642d60816ffca6561bb9497b25e5b94c
Nick Penkov <nick.penkov@gmail.com>
2024-05-01 10:07:49 +0200

    Add support for custom select command

--- lua/gfold/init.lua
+++ lua/gfold/init.lua
@@ -1,27 +1,32 @@
-local settings = require('gfold.settings')
-local utils = require('gfold.utils')
+local settings = require("gfold.settings")
+local utils = require("gfold.utils")
 
 local update_settings = function(user_opts)
-  utils.recursive_tbl_update(settings, user_opts)
+	utils.recursive_tbl_update(settings, user_opts)
 end
 
 local setup = function(user_opts)
-  update_settings(user_opts)
-  if settings.status.enable then
-    require('gfold.status').update_summary()
-  end
+	update_settings(user_opts)
+	if settings.status.enable then
+		require("gfold.status").update_summary()
+	end
 end
 
 local pick_repo = function(condition)
-  return require('gfold.picker').pick_repo(condition)
+	return require("gfold.picker").pick_repo(condition)
+end
+
+local pick_repo_and = function(condition, func)
+	return require("gfold.picker").pick_repo_and(condition, func)
 end
 
 local get_summary = function()
-  return require('gfold.status').get_summary()
+	return require("gfold.status").get_summary()
 end
 
 return {
-  setup = setup,
-  pick_repo = pick_repo,
-  get_summary = get_summary,
+	setup = setup,
+	pick_repo = pick_repo,
+	pick_repo_and = pick_repo_and,
+	get_summary = get_summary,
 }
--- lua/gfold/picker.lua
+++ lua/gfold/picker.lua
@@ -1,16 +1,28 @@
-local settings = require('gfold.settings')
-local get_repos = require('gfold.get').get_repos
+local settings = require("gfold.settings")
+local get_repos = require("gfold.get").get_repos
 
+-- This can wrap pick_repo_and with default on select function
 local pick_repo = function(condition)
-  get_repos(function(repos)
-    vim.ui.select(repos, {
-      prompt = 'gfold',
-      format_item = settings.picker.format_item,
-      kind = 'gfold',
-    }, settings.picker.on_select)
-  end, condition)
+	get_repos(function(repos)
+		vim.ui.select(repos, {
+			prompt = "gfold",
+			format_item = settings.picker.format_item,
+			kind = "gfold",
+		}, settings.picker.on_select)
+	end, condition)
+end
+
+local pick_repo_and = function(condition, func)
+	get_repos(function(repos)
+		vim.ui.select(repos, {
+			prompt = "gfold",
+			format_item = settings.picker.format_item,
+			kind = "gfold",
+		}, func)
+	end, condition)
 end
 
 return {
-  pick_repo = pick_repo,
+	pick_repo = pick_repo,
+	pick_repo_and = pick_repo_and,
 }
